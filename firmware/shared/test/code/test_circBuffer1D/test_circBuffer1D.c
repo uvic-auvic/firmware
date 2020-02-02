0d3d8af255d4437505ea1874d0dabebf18b55b26
Poorna Chander Kalidas <poornakalidas@gmail.com>
2020-02-01 22:16:46 -0800

    Added ability to build with device config

--- firmware/shared/test/code/test_circBuffer1D/test_circBuffer1D.c
+++ firmware/shared/test/code/test_circBuffer1D/test_circBuffer1D.c
@@ -1,4 +1,7 @@
+
 #include <cgreen/cgreen.h>
+#include "circBuffer1D.h"
+
 
 Describe(Cgreen);
 BeforeEach(Cgreen) {}
@@ -6,8 +9,12 @@ AfterEach(Cgreen) {}
 
 Ensure(test_circBuffer1D_addToBuffer)
 {
-    assert_that(1U, is_equal_to(1U));
+//    assert_that(1U, is_equal_to(0U));
+    uint8_t data[10U] = {0U};
+    bool ret = circBuffer1D_popByte(CIRCBUFFER1D_CHANNEL_TEST_1, data);
+    assert_that(ret, is_equal_to(1U));
 }
+
 
 int main(int argc, char **argv)
 {

fc3e5933834dce75f4da31400d00e327df2de078
Poorna Chander Kalidas <poornakalidas@gmail.com>
2020-02-07 13:59:29 -0800

    WIP

--- firmware/powerboard1/code/app/ADC.c
+++ firmware/powerboard1/code/app/ADC.c
@@ -1,11 +1,9 @@
 /*
- * LED.c
- *
- *  Created on: Feb  7, 2020
- *      Author: Poornachander
+ * ADC.c
  */
 
 #include "ADC.h"
+
 /* INCLUDES */
 #include "stm32f0xx.h"
 #include "RTOS.h"
@@ -22,19 +20,17 @@ typedef struct
 /* PRIVATE DATA DECLARAION */
 static ADC_data_S ADC_data;
 
+static const uint32_t ADC_softwareChannelToADCChannelMapping[ADC_CHANNEL_COUNT] =
+{
+    [ADC_CHANNEL_WATER_SENSOR] 			= ADC_Channel_0,
+    [ADC_CHANNEL_PRESSURE_SENSOR] 		= ADC_Channel_1,
+    [ADC_CHANNEL_LEFT_BATT_VOLTAGE] 	= ADC_Channel_2,
+    [ADC_CHANNEL_RIGHT_BATT_VOLTAGE] 	= ADC_Channel_3,
+    [ADC_CHANNEL_RIGHT_BATT_CURRENT] 	= ADC_Channel_4,
+    [ADC_CHANNEL_LEFT_BATT_CURRENT] 	= ADC_Channel_5,
+};
+
 /* PRIVATE FUNCTION DECLARATION */
-
-/* PRIVATE FUNCTION DEFINITION */
-// static void ADC_private_calibration (void)
-// {
-// 	ADC1->CR &= ~ADC_CR_ADEN;
-// 	ADC1->CR |= ADC_CR_ADCAL;//WHY WONT YOU TURN TO 1
-// 	while((ADC1->CR & ADC_CR_ADCAL) != 0)
-// 	{
-// 		//TIME-OUT MANAGEMENT
-// 	}
-// }
-
 static void ADC_private_initGPIOs(void)
 {
 	//Enable peripheral clock for GPIOA
@@ -43,11 +39,11 @@ static void ADC_private_initGPIOs(void)
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_StructInit(&GPIO_InitStructure);
 
-	GPIO_InitStructure.GPIO_Pin = (GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6| GPIO_Pin_7);
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitStructure.GPIO_Pin 	= GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Mode 	= GPIO_Mode_AN;
+	GPIO_InitStructure.GPIO_Speed 	= GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_PuPd 	= GPIO_PuPd_NOPULL;
+
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
@@ -58,26 +54,30 @@ static void ADC_private_initADC(void)
 	ADC_InitTypeDef ADC_InitStructure;
 	ADC_StructInit(&ADC_InitStructure);
 
-	ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
-	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
-	ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
-	ADC_InitStructure.ADC_ExternalTrigConv = 
-	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Left;
-	ADC_InitStructure.ADC_ScanDirection = ADC_ScanDirection_Upward;
-	
+	ADC_InitStructure.ADC_Resolution 		 	= ADC_Resolution_12b;
+	ADC_InitStructure.ADC_ContinuousConvMode 	= ENABLE;
+	ADC_InitStructure.ADC_ExternalTrigConvEdge 	= ADC_ExternalTrigConvEdge_None;
+	ADC_InitStructure.ADC_DataAlign 			= ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_ScanDirection 		= ADC_ScanDirection_Upward;
+
+	// Writes the settings above into the the ADC config registers
 	ADC_Init(ADC1, &ADC_InitStructure);
+
+	// Do calibration. Must be done before enabling the ADC
+	ADC_GetCalibrationFactor(ADC1);
+
+	// Choose which HW ADC channels to sample and the sampling rate.
+	for(ADC_channel_E channel = (ADC_channel_E)0U; channel < ADC_CHANNEL_COUNT; channel++)
+	{
+		ADC_ChannelConfig(ADC1, ADC_softwareChannelToADCChannelMapping[channel], ADC_SampleTime_28_5Cycles);
+	}
+
+	// Enable the ADC
 	ADC_Cmd(ADC1, ENABLE);
 
-	ADC_DMARequestModeConfig(ADC1, ENABLE);
+	// DMA will not stop after ADC first ADC conversion stops
+	ADC_DMARequestModeConfig(ADC1, ADC_DMAMode_Circular);
 	ADC_DMACmd(ADC1, ENABLE);
-
-	// ADC_private_calibration();
-	// ADC1->CR |= ADC_CR_ADEN;
-	// ADC1->CFGR1 |= ADC_CFGR1_CONT;
-	// ADC1->CHSELR |= ADC_CHSELR_CHSEL7	| ADC_CHSELR_CHSEL6
-	// 			 | ADC_CHSELR_CHSEL5 	| ADC_CHSELR_CHSEL4
-	// 			 | ADC_CHSELR_CHSEL3 	| ADC_CHSELR_CHSEL2
-	// 			 | ADC_CHSELR_CHSEL1 	| ADC_CHSELR_CHSEL0;
 }
 
 static void ADC_private_initDMA(void)
@@ -87,30 +87,23 @@ static void ADC_private_initDMA(void)
 	DMA_InitTypeDef DMA_InitStructure;
 	DMA_StructInit(&DMA_InitStructure);
 
-	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&(ADC1->DR);
-	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)ADC_data.rawData;
-	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
-	DMA_InitStructure.DMA_BufferSize = ADC_CHANNEL_COUNT;
-	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
-	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
-	DMA_InitStructure.DMA_Priority = DMA_Priority_Low;
-	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+	DMA_InitStructure.DMA_PeripheralBaseAddr 	= (uint32_t)&(ADC1->DR);
+	DMA_InitStructure.DMA_MemoryBaseAddr 		= (uint32_t)ADC_data.rawData;
+	DMA_InitStructure.DMA_DIR 					= DMA_DIR_PeripheralSRC;
+	DMA_InitStructure.DMA_BufferSize 			= ADC_CHANNEL_COUNT;
+	DMA_InitStructure.DMA_PeripheralInc 		= DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_MemoryInc 			= DMA_MemoryInc_Enable;
+	DMA_InitStructure.DMA_PeripheralDataSize 	= DMA_PeripheralDataSize_HalfWord;
+	DMA_InitStructure.DMA_MemoryDataSize 		= DMA_MemoryDataSize_HalfWord;
+	DMA_InitStructure.DMA_Mode 					= DMA_Mode_Circular;
+	DMA_InitStructure.DMA_Priority 				= DMA_Priority_Low;
+	DMA_InitStructure.DMA_M2M 					= DMA_M2M_Disable;
 
+	// Writes the settings above into the DMA config registers
 	DMA_Init(DMA1_Channel1, &DMA_InitStructure);
 
+	// Enable the DMA channel
 	DMA_Cmd(DMA1_Channel1, ENABLE);
-
-	// RCC->AHBENR |= RCC_AHBENR_DMA1EN;
-	// ADC1->CFGR1 |= ADC_CFGR1_DMAEN | ADC_CFGR1_DMACFG;
-	// DMA1_Channel1->CPAR = (uint32_t) (&(ADC1->DR));		//Peripheral Address
-	// DMA1_Channel1->CMAR = (uint32_t) ADC_data.rawData;		//Memory Address
-	// DMA1_Channel1->CNDTR = ADC_CHANNEL_COUNT;					//Memory Size
-	// DMA1_Channel1->CCR |= DMA_CCR_MINC		| DMA_CCR_MSIZE_0
-	// 				   | DMA_CCR_PSIZE_0 	| DMA_CCR_CIRC;
-	// DMA1_Channel1->CCR |= DMA_CCR_EN;
 }
 
 /* PUBLIC FUNCTIONS */
@@ -119,6 +112,9 @@ void ADC_init(void)
 	ADC_private_initGPIOs();
 	ADC_private_initDMA();
 	ADC_private_initADC();
+
+	// Kick off the first conversion
+	ADC_StartOfConversion(ADC1);
 }
 
 uint16_t ADC_getChannelData(const ADC_channel_E channel)

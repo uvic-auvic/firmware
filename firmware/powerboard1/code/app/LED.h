c46d60784f9bf5706fcfade009eae6a61585b777
Poorna Chander Kalidas <poornakalidas@gmail.com>
2020-02-07 07:32:51 -0800

    WIP

--- firmware/powerboard1/code/app/LED.h
+++ firmware/powerboard1/code/app/LED.h
@@ -5,9 +5,9 @@
  *      Author: Poornachander
  */
 
-#ifndef POWERBOARD1CODE_APP_LED_H_
-#define POWERBOARD1CODE_APP_LED_H_
+#ifndef POWERBOARD1_CODE_APP_LED_H_
+#define POWERBOARD1_CODE_APP_LED_H_
 
 void LED_init(void);
 void LED_run100ms(void);
-#endif /* POWERBOARD1CODE_APP_LED_H_ */
+#endif /* POWERBOARD1_CODE_APP_LED_H_ */

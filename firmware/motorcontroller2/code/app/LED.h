183a56e9fc4980cd5ade9df52d20f931c843f750
Poorna Chander Kalidas <poornakalidas@gmail.com>
2020-02-21 22:32:03 -0800

    Fixed header guards

--- firmware/motorcontroller2/code/app/LED.h
+++ firmware/motorcontroller2/code/app/LED.h
@@ -5,12 +5,12 @@
  *      Author: Poornachander
  */
 
-#ifndef MOTORCONTROLLER2_CODE_APP_LED_H_
-#define MOTORCONTROLLER2_CODE_APP_LED_H_
+#ifndef LED_H_
+#define LED_H_
 
 void LED_init(void);
 void LED_run15(void);
 void LED_run14(void);
 void LED_run13(void);
 void LED_run12(void);
-#endif /* MOTORCONTROLLER2_CODE_APP_LED_H_ */
+#endif /* LED_H_ */

c8b6095d67eb8fb9ee2a2eac8db0f80fa02b4194
cartercooper <47931010+cartercooper@users.noreply.github.com>
2020-05-14 21:54:20 -0700

    Carter (#27)
    * modified:   shared/code/circBuffer2D.c
    	modified:   shared/code/circBuffer2D.h
    	modified:   shared/test/code/test_circBuffer2D/circBuffer2D_componentSpecific.c
    	modified:   shared/test/code/test_circBuffer2D/test_circBuffer2D.c
    
    * Functional 2D Circular Buffer
    
    * Changes to be committed:
    	deleted:    libs/cgreen/gitrevision.h
    	modified:   shared/code/circBuffer2D.c

--- firmware/shared/code/circBuffer2D.c
+++ firmware/shared/code/circBuffer2D.c
@@ -4,14 +4,17 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 typedef struct
 {
 	uint8_t ** buffer;
 	uint16_t  head;
 	uint16_t  tail;
-	uint16_t  length;
-	uint16_t  count;
+	uint16_t  length;   //Length of slots
+	uint16_t  count;    //Number of slots in a channel
+	bool empty;
+
 } circBuffer2D_channelData_S;
 
 typedef struct
@@ -24,7 +27,7 @@ circBuffer2D_data_S circBuffer2D_data;
 extern circBuffer2D_config_S circBuffer2D_config[CIRCBUFFER2D_CHANNEL_COUNT];
 void circBuffer2D_init(void)
 {
-	for(circBuffer2D_channel_E channel = (circBuffer2D_channel_E)0U; channel < CIRCBUFFER2D_CHANNEL_COUNT;channel++)
+	for(circBuffer2D_channel_E channel = (circBuffer2D_channel_E)0U; channel < CIRCBUFFER2D_CHANNEL_COUNT; channel++)
 	{
 		circBuffer2D_config_S const * const channelConfig = &circBuffer2D_config[channel];
 		circBuffer2D_channelData_S * const channelData = &circBuffer2D_data.channelData[channel];
@@ -33,27 +36,35 @@ void circBuffer2D_init(void)
 			channelData->buffer = channelConfig->buffer;
 			channelData->length = channelConfig->length;
 			channelData->count = channelConfig->count;
+			channelData->empty = true;
 		} else
 		{
-			// ASSERT??
+			//ASSERT???
 		}
 	}
-
-	// do some additional checks to make sure there is no overlap in the memory regions
 }
 
-bool circBuffer2D_getSpaceAvailable(const circBuffer2D_channel_E channel)
+uint8_t circBuffer2D_getSpaceAvailable(const circBuffer2D_channel_E channel)
 {
-	bool ret = false;
+	uint8_t ret = 0;
 	if(channel < CIRCBUFFER2D_CHANNEL_COUNT)
 	{
+
 		circBuffer2D_channelData_S * channelData = &circBuffer2D_data.channelData[channel];
-		if(channelData->head > channelData->tail)
+		if(channelData->empty == false && channelData->head == channelData->tail ){
+			ret = 0;
+		}
+		else if (channelData->empty == true){
+			ret = channelData->count;
+		}
+		else if(channelData->head > channelData->tail)
 		{
-			ret = channelData->head - channelData->tail;
-		} else
+			ret = channelData->count - channelData->head - channelData->tail;
+		}
+		else if(channelData->tail > channelData->head)
 		{
 			ret = (channelData->head + channelData->count) - channelData->tail;
+		}else {
 		}
 	}
 
@@ -62,7 +73,9 @@ bool circBuffer2D_getSpaceAvailable(const circBuffer2D_channel_E channel)
 
 bool circBuffer2D_push(const circBuffer2D_channel_E channel, uint8_t const * const data, const uint16_t length)
 {
+
 	bool ret = false;
+
 	if((channel < CIRCBUFFER2D_CHANNEL_COUNT) && (data != NULL) && (length > 0))
 	{
 		circBuffer2D_channelData_S * channelData = &circBuffer2D_data.channelData[channel];
@@ -70,8 +83,9 @@ bool circBuffer2D_push(const circBuffer2D_channel_E channel, uint8_t const * con
 		{
 			memset(&channelData->buffer[channelData->head], 0U, channelData->length);
 			memcpy(&channelData->buffer[channelData->head], data, length);
-			channelData->head = (channelData->head + 1U) % channelData->count;
+			channelData->head = (channelData->head + 1) % (channelData->count);
 			ret = true;
+			channelData->empty = false;
 		}
 	}
 
@@ -83,12 +97,20 @@ uint8_t circBuffer2D_pop(const circBuffer2D_channel_E channel, uint8_t * const d
 	uint8_t ret = 0U;
 	if((channel < CIRCBUFFER2D_CHANNEL_COUNT) && (dataToReturn != NULL))
 	{
+		
 		circBuffer2D_channelData_S * channelData = &circBuffer2D_data.channelData[channel];
-		if(channelData->head != channelData->tail)
+		if(channelData->head != channelData->tail || channelData->empty == false)
 		{
+			
 			memset(dataToReturn, 0U, channelData->length);
 			memcpy(dataToReturn, &channelData->buffer[channelData->tail], channelData->length);
-			channelData->tail = (channelData->tail + 1U) % channelData->count;
+			channelData->tail = (channelData->tail + 1U)  % (channelData->count);
+
+			if(channelData->head == channelData->tail)
+			{
+				channelData->empty = true;
+			}
+
 			ret = channelData->length;
 		}
 	}
--- firmware/shared/test/code/test_circBuffer2D/circBuffer2D_componentSpecific.c
+++ firmware/shared/test/code/test_circBuffer2D/circBuffer2D_componentSpecific.c
@@ -1,4 +1,4 @@
-	/*
+/*
  * circBuffer2D_componentSpecific.c
  *
  *  Created on: Jul 22, 2019
@@ -7,14 +7,22 @@
 
 #include "circBuffer2D.h"
 
-static uint8_t circBuffer2D_buffer_TEST1[5U][5U];
+static uint8_t circBuffer2D_buffer_TEST1[8U][8U];
+static uint8_t circBuffer2D_buffer_TEST2[8U][8U];
 
 const circBuffer2D_config_S circBuffer2D_config[CIRCBUFFER2D_CHANNEL_COUNT] =
 {
 	[CIRCBUFFER2D_CHANNEL_TEST_1] =
 	{
 		.buffer = (uint8_t **)&circBuffer2D_buffer_TEST1[0U],
-		.length = 5U,
-		.count = 5U,
+		.length = 8U,
+		.count = 8U,
+	},
+
+	[CIRCBUFFER2D_CHANNEL_TEST_2] =
+	{
+		.buffer = (uint8_t **)&circBuffer2D_buffer_TEST2[0U],
+		.length = 8U,
+		.count = 8U,
 	}
 };

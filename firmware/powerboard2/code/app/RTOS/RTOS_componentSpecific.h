2853c26bffcbe62b7096f56fe178792f6b5ed60d
Poorna Chander Kalidas <poornakalidas@gmail.com>
2020-08-05 00:44:50 -0600

    Added Power Board 2 as a component. Blinkemss!

---
+++ firmware/powerboard2/code/app/RTOS/RTOS_componentSpecific.h
@@ -0,0 +1,17 @@
+/*
+ * RTOS_componentSpecific.h
+ *
+ *  Created on: Aug 5, 2020
+ *      Author: Poornachander
+ */
+
+#ifndef RTOS_COMPONENTSPECIFIC_H_
+#define RTOS_COMPONENTSPECIFIC_H_
+
+void RTOS_init(void);
+void RTOS_run1ms(void);
+void RTOS_run10ms(void);
+void RTOS_run100ms(void);
+void RTOS_run1000ms(void);
+
+#endif /* RTOS_COMPONENTSPECIFIC_H_ */

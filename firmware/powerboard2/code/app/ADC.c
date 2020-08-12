a468fa29f7ac597a09803f8ff71befad8216d281
shtan <51217607+Shuhangtan@users.noreply.github.com>
2020-08-11 21:02:22 -0700

    Code Cleanup

--- firmware/powerboard2/code/app/ADC.c
+++ firmware/powerboard2/code/app/ADC.c
@@ -61,19 +61,11 @@ static void ADC_private_initGPIOs(void)
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
     GPIO_InitStructure.GPIO_Pin     = GPIO_Pin_0 | GPIO_Pin_1;
-    GPIO_InitStructure.GPIO_Mode    = GPIO_Mode_AN;
-    GPIO_InitStructure.GPIO_OType   = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_Speed   = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_PuPd    = GPIO_PuPd_NOPULL;
-
+    
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 
     GPIO_InitStructure.GPIO_Pin     = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2;
-    GPIO_InitStructure.GPIO_Mode    = GPIO_Mode_AN;
-    GPIO_InitStructure.GPIO_OType   = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_Speed   = GPIO_Speed_50MHz;
-    GPIO_InitStructure.GPIO_PuPd    = GPIO_PuPd_NOPULL;
-
+    
     GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
